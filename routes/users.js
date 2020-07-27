073837b719ebc2ad9dcd153d393b60e2d979092e
Tejas Dahad <tejasdahad000@gmail.com>
2020-07-27 11:20:22 +0530

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+const { check, validationResult } = require('express-validator/check');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+router.post('/', [
+    check('name','Please enter name').not().isEmpty(),
+    check('email','Please enter email').isEmail(),
+    check('password','Please enter password of min 5 letters').isLength({min:5})
+],async (req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const { name, email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+
+        if(user) {
+            return res.status(400).json({msg: 'user already exists'});
+        }
+
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = {
+            user : {
+                id: user.id
+            }
+        };
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn:3600
+        },(err, token) => {
+            if(err) throw err;
+            res.json({token});
+        });
+
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,18 @@
+const PORT = process.env.PORT || 5000;
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.use(express.json({extended:false}));
+
+app.get('/',(req, res) => {
+    res.json({ msg: 'Hello' });
+})
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+
+app.listen(PORT,() => console.log(`Server started on port ${PORT}`));
