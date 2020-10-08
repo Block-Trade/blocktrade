7c3901c3c2b087fff632d5fc5782601122dabac1
Tejas Dahad <56474128+tejasdahad@users.noreply.github.com>
2020-10-08 18:00:38 +0530

    Middleware updated

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const config = require('config');
 
 module.exports = function(req, res, next) {
     // Get the token
-    const token = req.header('x-auth-token');
+    const token = req.header('token');
 
     if(!token) {
         return res.status(401).json({msg:'Authorization denied'});
