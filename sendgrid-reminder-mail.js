6245440197c305429ae0f829e33d1d8f096ac5ff
karan kangude <karakankangude17@gmail.com>
2020-11-03 20:28:48 +0530

    reminder mail template added

--- sendgrid-reminder-mail.js
+++ sendgrid-reminder-mail.js
@@ -2,16 +2,17 @@ const sgMail = require('@sendgrid/mail');
 
 sgMail.setApiKey(process.env.SG_SENDGRID_API_KEY.toString());
 sgMail.setSubstitutionWrappers('{{', '}}');
-const sendReminderMail = async ({mailCont}) => {
+const sendReminderMail = async ({ mailCont }) => {
   const msg = {
     to: mailCont.email,
     from: 'karankangude17@gmail.com',
     text: 'Blocktrade',
     subject: 'Payment Reminder',
     html: `<h3>We kindly remind you about your payment to be done </h3>
-           <p>Trade Id: ${mailCont.tradeId}</p>
-           <p>Username: ${mailCont.username}</p>
+           <p>Trade Id: <b>${mailCont.TradeId}</b></p>
+           <p>Username: <b>${mailCont.username}</b></p>
            <p>Amount : <b>${mailCont.amount}</b></p>`,
+    templateId: '386125ad-3200-4e32-b661-e95117039658',
     substitutions: {
       token: mailCont.token,
     },
