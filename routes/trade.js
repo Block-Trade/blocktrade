a13af17e2c277c1aeb4e28a1e139ea5efe187638
Karan Kangude <karankangude17@gmail.com>
2021-03-01 16:09:59 +0530

    routes in trade.js updated

--- routes/trade.js
+++ routes/trade.js
@@ -9,8 +9,13 @@ const { Timestamp } = require('mongodb');
 router.get('/', auth, async (req, res) => {
   try {
     const userId = await User.findById(req.body.user.id);
-    console.log(userId)
-    const trade = await Trade.find({$or:[{exporterUserName:userId.username},{importerUserName:userId.username}]});
+    console.log(userId);
+    const trade = await Trade.find({
+      $or: [
+        { exporterUserName: userId.username },
+        { importerUserName: userId.username },
+      ],
+    });
     console.log(trade);
     return res.status(200).json({ trades: trade });
   } catch (e) {
@@ -18,58 +23,68 @@ router.get('/', auth, async (req, res) => {
   }
 });
 
-router.post('/username',auth,async(req,res) => {
+router.post('/username', auth, async (req, res) => {
   try {
-    const user = await User.findOne({username: req.body.username});
-    if(!user){
-      return res.status(400).json({msg:"User not found"});
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(400).json({ msg: 'User not found' });
     }
-    return res.status(200).json({user});
-  } catch (e) {
-  
-  }
+    return res.status(200).json({ user });
+  } catch (e) {}
 });
 
-router.post('/',auth,async(req,res) => {
-  try{
-    const { user, expUser,impUser,inco,creditPeriod,amount,paymentType, invoiceDate } = req.body;
+router.post('/', auth, async (req, res) => {
+  try {
+    const {
+      user,
+      expUser,
+      impUser,
+      inco,
+      creditPeriod,
+      amount,
+      paymentType,
+      invoiceDate,
+    } = req.body;
     console.log(new Date().getTime());
-    var a = expUser+impUser+new Date().getTime();
+    var a = expUser + impUser + new Date().getTime();
     const tradeId = crypto.createHash('sha256').update(a).digest('base64');
     console.log(tradeId);
     const trade1 = new Trade({
-      incoterms:inco,
+      incoterms: inco,
       creditPeriod,
       paymentType,
       amount,
       invoiceDate,
       TradeId: tradeId,
-      exporterUserName:expUser,
+      exporterUserName: expUser,
       importerUserName: impUser,
-      tradeStatus: 'DU'
+      tradeStatus: 'DU',
     });
     await trade1.save();
-    res.status(200).send({trade1});
-  }catch(e){
-    res.status(400).send({e});
+    res.status(200).send({ trade1 });
+  } catch (e) {
+    res.status(400).send({ e });
   }
 });
-router.post('/update',auth,async(req,res) => {
-  try{
-    const { tradeId,tradeStatus } = req.body;
-    const trade =await Trade.update({TradeId: tradeId},{tradeStatus:tradeStatus});
-    res.status(200).send({trade});
-  }catch(e){
-    res.status(400).send({e});
+router.post('/update', auth, async (req, res) => {
+  try {
+    const { tradeId, tradeStatus } = req.body;
+    const trade = await Trade.update(
+      { TradeId: tradeId },
+      { tradeStatus: tradeStatus }
+    );
+    res.status(200).send({ trade });
+  } catch (e) {
+    res.status(400).send({ e });
   }
 });
-router.post('/delete',auth,async(req,res) => {
-  try{
+router.post('/delete', auth, async (req, res) => {
+  try {
     const { tradeId } = req.body;
-    const trade =await Trade.remove({TradeId: tradeId});
+    const trade = await Trade.remove({ TradeId: tradeId });
     res.status(200).json({ msg: 'Trade deleted successfully' });
-  }catch(e){
-    res.status(400).send({e});
+  } catch (e) {
+    res.status(400).send({ e });
   }
 });
 module.exports = router;
