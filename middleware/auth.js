d9afbf2b40f3ef8eca836ae0ace354264fa9dfd1
Karan Kangude <karankangude17@gmail.com>
2021-03-01 16:12:10 +0530

    auth changes alignment fixes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,19 @@
 const jwt = require('jsonwebtoken');
-const config = require('config');
 
-module.exports = function(req, res, next) {
-    // Get the token
-    const token = req.header('token');
-    //console.log(token);
-    if(!token) {
-        return res.status(401).json({msg:'Authorization denied'});
-    }
-    try {
-        const decoded = jwt.verify(token, process.env.JWTSECRET);
+module.exports = function (req, res, next) {
+  // Get the token
+  const token = req.header('token');
+  //console.log(token);
+  if (!token) {
+    return res.status(401).json({ msg: 'Authorization denied' });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWTSECRET);
 
-        req.body.user = decoded.user;
-        console.log(req.body.user);
-        next();
-    } catch (error) {
-        res.status(401).json({msg: 'Token invalid'});
-    }
-    
-}
+    req.body.user = decoded.user;
+    console.log(req.body.user);
+    next();
+  } catch (error) {
+    res.status(401).json({ msg: 'Token invalid' });
+  }
+};
